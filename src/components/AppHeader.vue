19170cf32ee6a31cbfe6b4d904171275cf2da40d
Bruno Canini <brunocanini1994@gmail.com>
2023-05-30 17:07:42 +0200

    asset img

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+
+import TheSearchbar from './TheSearchbar.vue'
+
+export default {
+  name: "AppHeader",
+  components:{
+    TheSearchbar,
+  },
+}
+
+
+</script>
+
+<template>
+    <TheSearchbar />
+
+</template>
+
+<style lang="scss" scoped>
+@use '../style/variables.scss' as *;
+
+
+</style>
---
+++ src/components/TheSearchbar.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+  name: "TheSearchbar",
+}
+
+</script>
+
+<template>
+
+    <div class="containerSearchbar">
+
+        <div class="boxLogo"></div>
+        <div class="boxLink"></div>
+        <div class="boxButton"></div>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../style/variables.scss' as *;
+
+.containerSearchbar{
+    height: 115px;
+    width: 100%;
+    border: 1px solid black;
+}
+
+
+</style>
