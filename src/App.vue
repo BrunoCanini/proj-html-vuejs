f15cc2e9e96a04d7c896345b8ffeb45a604adb39
Bruno Canini <brunocanini1994@gmail.com>
2023-05-30 16:49:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../src/style/variables.scss' as *;
+
+
+
+
+</style>
